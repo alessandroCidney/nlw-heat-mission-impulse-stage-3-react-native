bda0e4e181792f7a836c85a782bff3827456ad77
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2022-01-29 16:34:43 -0300

    <new> Created SendMessageForm component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, View } from "react-native";
 import { Header } from "../../components/Header";
 import { MessageList } from "../../components/MessageList";
+import { SendMessageForm } from "../../components/SendMessageForm";
 import { SignInBox } from "../../components/SignInBox";
 import { styles } from "./styles";
 
@@ -13,7 +14,7 @@ export function Home () {
 
       <MessageList />
 
-      <SignInBox />
+      <SendMessageForm />
     </View>
   );
 };
